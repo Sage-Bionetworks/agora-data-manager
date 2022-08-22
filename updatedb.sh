c2e55c982f9d1775282ee5b6e96ee3c0ec0227ea
Khai Do <zaro0508@gmail.com>
2022-08-22 14:19:42 -0700

    use variables for bastian

--- updatedb.sh
+++ updatedb.sh
@@ -16,14 +16,18 @@ SYNAPSE_PASSWORD_ESC="'${SYNAPSE_PASSWORD//\'/$q_mid}'"
 DB_USER_ESC="'${DB_USER//\'/$q_mid}'"
 DB_PASS_ESC="'${DB_PASS//\'/$q_mid}'"
 
+# bastian configs
+BASTIAN_USER="ec2-user"
+BASTIAN_SSH_KEY="~/.ssh/agora-ci-$TRAVIS_BRANCH.pem"
+
 # clean up from previous builds
-ssh -i ~/.ssh/agora-travis.pem ec2-user@$BASTIAN_HOST "rm -rf /tmp/work"
+ssh -i $BASTIAN_SSH_KEY $BASTIAN_USER@$BASTIAN_HOST "rm -rf /tmp/work"
 
 # create directories for data
-ssh -i ~/.ssh/agora-travis.pem ec2-user@$BASTIAN_HOST "mkdir -p /tmp/work/data/team_images"
+ssh -i $BASTIAN_SSH_KEY $BASTIAN_USER@$BASTIAN_HOST "mkdir -p /tmp/work/data/team_images"
 
 # setup script on bastian
-scp -i ~/.ssh/agora-travis.pem import-data.sh data-manifest.json create-indexes.js ec2-user@$BASTIAN_HOST:/tmp/work/.
+scp -i $BASTIAN_SSH_KEY import-data.sh data-manifest.json create-indexes.js $BASTIAN_USER@$BASTIAN_HOST:/tmp/work/.
 
 # run import on bastian
-ssh -i ~/.ssh/agora-travis.pem ec2-user@$BASTIAN_HOST "bash /tmp/work/import-data.sh $TRAVIS_BRANCH $SYNAPSE_USERNAME_ESC $SYNAPSE_PASSWORD_ESC $DB_HOST $DB_USER_ESC $DB_PASS_ESC"
+ssh -i $BASTIAN_SSH_KEY $BASTIAN_USER@$BASTIAN_HOST "bash /tmp/work/import-data.sh $TRAVIS_BRANCH $SYNAPSE_USERNAME_ESC $SYNAPSE_PASSWORD_ESC $DB_HOST $DB_USER_ESC $DB_PASS_ESC"
